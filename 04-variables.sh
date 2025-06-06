c9aebdd13598e2444d1f1d8dd812e3c098b2d9a6
srikanthbaireddy <srikanth.baireddy@outlook.com>
2025-06-06 12:27:43 +0530

    hello world created

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hey $PERSON2, how are you ?"  
+echo "$PERSON2:: Hey $PERSON1, I am fine whatsup"
+echo "$PERSON1:: I am fine, whats going on"
+echo "$PERSON2:: I am going to Mars, will you come along with me"
+echo "$PERSON1:: Sorry, you carry on I will come once you are back $PERSON2"
