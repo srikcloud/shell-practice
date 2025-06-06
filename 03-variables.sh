8945ec6b3c33fd4d8f89493083bee84cc2322395
srikanthbaireddy <srikanth.baireddy@outlook.com>
2025-06-06 12:22:02 +0530

    hello world created

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,8 +3,8 @@
 PERSON1=MODI
 PERSON2=MUSK
 
-echo "$PERSON1:: "Hey $PERSON2, how are you ?"  
-echo "$PERSON2:: "Hey $PERSON1, I am fine whatsup"
-echo "$PERSON1:: "I am fine, whats going on"
-echo "$PERSON2:: "I am going to Mars, will you come along with me"
-echo "$PERSON1:: "Sorry, you carry on I cannot come"
+echo "$PERSON1:: Hey $PERSON2, how are you ?"  
+echo "$PERSON2:: Hey $PERSON1, I am fine whatsup"
+echo "$PERSON1:: I am fine, whats going on"
+echo "$PERSON2:: I am going to Mars, will you come along with me"
+echo "$PERSON1:: Sorry, you carry on I cannot come"
