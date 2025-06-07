cf040b99a655246bc6572b90948b55d928c534b8
srikanthbaireddy <srikanth.baireddy@outlook.com>
2025-06-07 20:57:06 +0530

    Special Vars

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -2,11 +2,11 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
    echo "ERROR:: Please run this script with root access"
 else
-   echo "you are logged in with root access"
+   echo "you are running with root access"
 fi
 
 dnf install mysql -y
